4ac25be1cfeff5867a286b7ae2582e511f8878b9
aaegly <32500812+aaegly@users.noreply.github.com>
2018-10-24 19:19:30 -0700

    final implementation of RSA
    used rabin-miller as well as checking low primes from the reading

--- rsa.py
+++ rsa.py
@@ -1,125 +1,205 @@
 __author__ = 'andrewegly'
-'''
-620031587
-Net-Centric Computing Assignment
-Part A - RSA Encryption
-'''
+"""
+Andrew Egly
+Crypto RSA implementation
+Project 2
+"""
 import random
+import sys
+import math
 
-'''
-Euclid's algorithm for determining the greatest common divisor
-Use iteration to make it faster for larger integers
-'''
+from random import randrange
+
+# determines whether a given number is prime
+def rabinMiller(n, k=10):
+    if n == 2:
+            return True
+    if not n & 1:
+            return False
+
+    def check(a, s, d, n):
+            x = pow(a, d, n)
+            if x == 1:
+                    return True
+            for i in range(1, s - 1):
+                    if x == n - 1:
+                            return True
+                    x = pow(x, 2, n)
+            return x == n - 1
+
+    s = 0
+    d = n - 1
+
+    while d % 2 == 0:
+            d >>= 1
+            s += 1
+
+    for i in range(1, k):
+            a = randrange(2, n - 1)
+            if not check(a, s, d, n):
+                    return False
+    return True
+
+# detects lower primes without having to use rabinMiller
+def isPrime(n):
+     #lowPrimes is all primes (sans 2, which is covered by the bitwise and operator)
+     #under 1000.
+     # taking n modulo each lowPrime allows us to remove a huge chunk
+     #of composite numbers from our potential pool without resorting to Rabin-Miller
+     lowPrimes =   [3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
+                   ,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179
+                   ,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269
+                   ,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367
+                   ,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461
+                   ,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571
+                   ,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661
+                   ,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773
+                   ,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883
+                   ,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997]
+     if (n >= 3):
+         if (n&1 != 0):
+             for p in lowPrimes:
+                 if (n == p):
+                    return True
+                 if (n % p == 0):
+                     return False
+             return rabinMiller(n)
+     return False
+
+# generates a large prime number for making the keys
+def generateLargePrime(k):
+     # k is the desired bit length
+     r = 100*(math.log(k,2)+1) #number of attempts max
+     r_ = r
+     while r>0:
+        # randrange is mersenne twister and is completely deterministic, so it shouldn't be used
+        # cryptographically
+         n = random.randrange(2**(k-1),2**(k))
+         r -= 1
+         if isPrime(n) == True:
+             return n
+
+     str_failure = "Failure after" + str(r_) + "tries."
+     return str_failure
 
 
 def gcd(a, b):
+    """
+    Euclid's algorithm for determining the greatest common divisor
+    """
     while b != 0:
         a, b = b, a % b
     return a
 
+def multiplicative_inverse(a, b):
+    """
+    Returns a tuple (r, i, j) such that r = gcd(a, b) = ia + jb
+    """
+    # r = gcd(a,b) i = multiplicitive inverse of a mod b
+    #      or      j = multiplicitive inverse of b mod a
+    x = 0
+    y = 1
+    lx = 1
+    ly = 0
+    oa = a  # Remember original a/b to remove
+    ob = b  # negative values from return results
+    while b != 0:
+        q = a // b
+        (a, b) = (b, a % b)
+        (x, lx) = ((lx - (q * x)), x)
+        (y, ly) = ((ly - (q * y)), y)
+    if lx < 0:
+        lx += ob  # If negative wrap modulo orignal b
+    if ly < 0:
+        ly += oa  # If negative wrap modulo orignal a
+    # return a , lx, ly  # Return only positive values
+    return lx
 
-'''
-Euclid's extended algorithm for finding the multiplicative inverse of two numbers
-'''
+def multiply(x, y):
+    _CUTOFF = 1536
+    if x.bit_length() <= _CUTOFF or y.bit_length() <= _CUTOFF:  # Base case
+        return x * y
+    else:
+        n = max(x.bit_length(), y.bit_length())
+        half = (n + 32) // 64 * 32
+        mask = (1 << half) - 1
+        xlow = x & mask
+        ylow = y & mask
+        xhigh = x >> half
+        yhigh = y >> half
 
+        a = multiply(xhigh, yhigh)
+        b = multiply(xlow + xhigh, ylow + yhigh)
+        c = multiply(xlow, ylow)
+        d = b - a - c
+        return (((a << half) + d) << half) + c
 
-def multiplicative_inverse(e, phi):
-    d = 0
-    x1 = 0
-    x2 = 1
-    y1 = 1
-    temp_phi = phi
+# generating public/private keypairs
+def generate_keypair(keySize=10):
+    p = generateLargePrime(keySize)
+    print(p)
+    q = generateLargePrime(keySize)
+    print(q)
 
-    while e > 0:
-        temp1 = temp_phi / e
-        temp2 = temp_phi - temp1 * e
-        temp_phi = e
-        e = temp2
-
-        x = x2 - temp1 * x1
-        y = d - temp1 * y1
-
-        x2 = x1
-        x1 = x
-        d = y1
-        y1 = y
-
-    if temp_phi == 1:
-        return d + phi
-
-
-'''
-Tests to see if a number is prime.
-'''
-
-
-def is_prime(num):
-    if num == 2:
-        return True
-    if num < 2 or num % 2 == 0:
-        return False
-    for n in range(3, int(num ** 0.5) + 2, 2):
-        if num % n == 0:
-            return False
-        return True
-
-
-def generate_keypair(p, q):
-    if not (is_prime(p) and is_prime(q)):
-        raise ValueError('Both numbers must be prime.')
-    elif p == q:
+    if p == q:
         raise ValueError('p and q cannot be equal')
-    # n = pq
-    n = p * q
-    # Phi is the totient of n
-    phi = (p - 1) * (q - 1)
-    # Choose an integer e such that e and phi(n) are coprime
+
+    #n = pq
+    n = multiply(p, q)
+
+    #Phi is the totient of n
+    phi = multiply((p-1),(q-1))
+
+    #Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
-    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
+
+    #Use Euclid's Algorithm to verify that e and phi(n) are comprime
     g = gcd(e, phi)
+
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
-        # Use Extended Euclid's Algorithm to generate the private key
+
+    #Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
 
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
+    #Return public and private keypair
+    #Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
-
 def encrypt(pk, plaintext):
-    # Unpack the key into it's components
+    #Unpack the key into it's components
     key, n = pk
-    # Convert each letter in the plaintext to numbers based on the character using a^b mod m
+    #Convert each letter in the plaintext to numbers based on the character using a^b mod m
     cipher = [(ord(char) ** key) % n for char in plaintext]
-    # Return the array of bytes
+    #Return the array of bytes
     return cipher
 
 
 def decrypt(pk, ciphertext):
-    # Unpack the key into its components
+    #Unpack the key into its components
     key, n = pk
-    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    #Generate the plaintext based on the ciphertext and key using a^b mod m
     plain = [chr((char ** key) % n) for char in ciphertext]
-    # Return the array of bytes as a string
+    #Return the array of bytes as a string
     return ''.join(plain)
-
 
 if __name__ == '__main__':
     '''
     Detect if the script is being run directly by the user
     '''
-    print("RSA Encrypter/ Decrypter")
-    p = int(input("Enter a prime number (17, 19, 23, etc): "))
-    q = int(input("Enter another prime number (Not one you entered above): "))
-    print("Generating your public/private keypairs now . . .")
-    public, private = generate_keypair(p, q)
-    print("Your public key is ", public, " and your private key is ", private)
-    message = input("Enter a message to encrypt with your private key: ")
-    encrypted_msg = encrypt(private, message)
-    print("Your encrypted message is: ")
-    print(''.join(map(lambda x: str(x), encrypted_msg)))
-    print("Decrypting message with public key ", public, " . . .")
-    print("Your message is:")
-    print(decrypt(public, encrypted_msg))
+    print("Running RSA algorithm test")
+
+    print("Generating public/private keypairs now . . .")
+    public, private = generate_keypair()
+    print("Public key is ", public ," and Private key is ", private)
+
+    message = str(sys.argv[1])
+
+    e_msg = encrypt(private, message)
+
+    print("Encrypted message is: ")
+    print(''.join(map(lambda x: str(x), e_msg)))
+    print("Decrypting message ", public ,"...")
+    print("Decrypted message is:")
+    print(decrypt(public, e_msg))
